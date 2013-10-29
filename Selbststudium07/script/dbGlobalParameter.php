9bb38ec9d8590fe842726af0383b8694cca28800
LordCoffee <christian.koller@gmx.ch>
2013-10-29 06:25:52 +0100

    change set up of config data

--- Selbststudium07/script/dbGlobalParameter.php
+++ Selbststudium07/script/dbGlobalParameter.php
@@ -1,15 +1,15 @@
 <?php
 	
 	// local settings in test environment
-	
+	/*
 	define("DB_CONNECTION", "mysql:host=localhost;dbname=dbWeb");
 	define("DB_USER", "root");
 	define("DB_PASSWORD", "rootpass");
-	
+	*/
 	// required environment by given assignment
-	/*
+	
 	define("DB_CONNECTION", "mysql:host=localhost;dbname=loc_orm");
 	define("DB_USER", "loc_orm");
 	define("DB_PASSWORD", "12341234");
-	*/						
+							
 ?>
